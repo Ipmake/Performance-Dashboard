7a248311f1ccb2c752b2cf4fa23c9e6edb223e96
Ipmake123 <niko.denis727@gmail.com>
2022-04-30 19:17:39 +0200

    update

--- src/pages/dashboard/renderer.js
+++ src/pages/dashboard/renderer.js
@@ -1,34 +1,33 @@
 const os = require("os");
 const osu = require("node-os-utils");
 const si = require('systeminformation');
+const cpuStat = require('cpu-stat');
 
+//----------------------------------------------------------------
 //get elements
-
 //cpu
-const cpuUSAGE = document.getElementById("charts-cpu-USAGE")
-const cpuTEMP = document.getElementById("charts-cpu-TEMPERATURE")
-const cpuSPEED = document.getElementById("charts-cpu-SPEED")
-const cpuVOLTAGE = document.getElementById("charts-cpu-VOLTAGE")
+const cpuUSAGE = document.getElementById("chart-cpu-USAGE")
+const cpuTEMP = document.getElementById("chart-cpu-TEMPERATURE")
+const cpuSPEED = document.getElementById("chart-cpu-SPEED")
+const cpuVOLTAGE = document.getElementById("chart-cpu-VOLTAGE")
+
+const cpuInfo = document.getElementById("cpu-name")
 
 //memory
-const memUSAGE = document.getElementById("charts-mem-USAGE")
-const memVoltage = document.getElementById("charts-mem-VOTAGE")
+const memUSAGE = document.getElementById("chart-mem-USAGE")
+const memVoltage = document.getElementById("chart-mem-VOTAGE")
 
 //gpu
-const gpuUSAGE = document.getElementById("charts-gpu-USAGE")
-const gpuVRAM = document.getElementById("charts-gpu-VRAM")
-const gpuTEMP = document.getElementById("charts-gpu-TEMPERATURE")
+const gpuUSAGE = document.getElementById("chart-gpu-USAGE")
+const gpuVRAM = document.getElementById("chart-gpu-VRAM")
+const gpuTEMP = document.getElementById("chart-gpu-TEMPERATURE")
 
+const gpuInfo = document.getElementById("gpu-name")
+//----------------------------------------------------------------
 
-//cpu data
-var cpu = null;
-si.cpu().then(data => { 
-    cpu = data;
+getHardwareInfo() //get hardware info 
 
-});
-
-
-
+var refreshes = 0
 
 
 update()
@@ -37,16 +36,49 @@ sync()
 function sync() {
     //cpu
 
-
-    setTimeout(sync, 200)
 }
 
 async function update() {
+    osu.cpu.usage().then(async data => {
+        cpuUSAGE.style = `--percentage : ${data}; --fill: #4E5D94 ;`
+        cpuUSAGE.innerHTML = `${Math.round(data)}%`
+    })
+    si.cpu().then(async data => {
+        if(data.voltage) {
+            cpuVOLTAGE.style = `--percentage : ${data.voltage / 2 * 100}; --fill: #4E5D94 ;`
+            cpuVOLTAGE.innerHTML = `${data.voltage}V / 2V`
+        } else {
+            cpuVOLTAGE.innerHTML = `Not supported`
+            if(refreshes > 5) {
+                cpuVOLTAGE.style = `--percentage : 0; --fill: #4E5D94; width: 0px; opacity: 0;`
+                cpuVOLTAGE.parentElement.style = `width: 0px; opacity: 0;`
+            }
+        }
 
-    let gpu = (await si.graphics()).controllers[0]
-    console.log(gpu)
-    gpuUsage = gpu.memoryUsed / await gpu.memoryTotal * 100
+        const osdata = cpuStat.clockMHz(0);
+        cpuSPEED.style = `--percentage : ${Math.round(osdata / 100) / 10 / data.speedMax * 100}; --fill:   #4E5D94 ;`
+        cpuSPEED.innerHTML = `${Math.round(osdata / 100) / 10}GHz / ${data.speedMax}GHz`
+        refreshes++;
+    })
+    si.cpuTemperature().then(temp => {
+        if(!temp.main){
+            cpuTEMP.innerHTML = `Not supported`
+            if(refreshes > 5) {
+                cpuTEMP.style = `--percentage : 0; --fill: #4E5D94; width: 0px; opacity: 0;`
+                cpuTEMP.parentElement.style = `width: 0px; opacity: 0;`
+            }
+            return
+        }
+        cpuTEMP.style = `--percentage : ${temp.main}; --fill: #4E5D94 ;`
+        cpuTEMP.innerHTML = `${Math.round(temp.main)}°C`
+    })
     
 
     setTimeout(update, 2000)
 }   
+
+
+async function getHardwareInfo(){
+    si.cpu().then(data => cpuInfo.innerHTML = data.brand)
+    si.graphics().then(data => gpuInfo.innerHTML = data.controllers[0].model)
+}
